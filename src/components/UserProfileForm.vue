6596668db471e0c83884d3a65f8b27b956002010
Steven SHIH <steven0210423@gmail.com>
2025-03-26 15:12:43 +0800

    3/26 使用者個人資訊瀏覽畫面實現出來 https://trello.com/c/Nq2AcFYV/27-%E8%BA%AB%E7%82%BA%E3%80%90%E5%89%8D%E7%AB%AF%E3%80%91%EF%BC%8C%E6%88%91%E9%9C%80%E8%A6%81%E5%B0%87%E3%80%90%E4%BD%BF%E7%94%A8%E8%80%85%E5%80%8B%E4%BA%BA%E8%B3%87%E8%A8%8A%E7%80%8F%E8%A6%BD%E7%95%AB%E9%9D%A2%E5%AF%A6%E7%8F%BE%E5%87%BA%E4%BE%86%E3%80%91

--- src/components/UserProfileForm.vue
+++ src/components/UserProfileForm.vue
@@ -107,7 +107,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 h2{
   text-align: center;
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import UserView from '../views/UserView.vue'
 import ErrorModal from '../components/ErrorModal.vue'
 
 const routes = [
@@ -28,6 +29,11 @@ const routes = [
     path: '/register',
     name: 'register',
     component: RegisterView
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: UserView
   }
 ]
 
